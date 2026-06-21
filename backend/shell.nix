fbf73eb3048072a6099df7216abc1c12571b81ee
Pedro Alves <pedropaula2325p@gmail.com>
2026-06-21 11:28:52 -0300

    feat: adiciona nix-shell para desenvolvimento

---
+++ backend/shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    nodejs_20
+    openssl
+    prisma-engines
+  ];
+
+  shellHook = ''
+    export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+    export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+    export PRISMA_QUERY_ENGINE_ROUTING_URL="${pkgs.prisma-engines}/bin/query-engine"
+    export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+    
+    echo "🚀 Ambiente NestJS + Prisma pronto no NixOS!"
+  '';
+}
